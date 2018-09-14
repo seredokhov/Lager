747bc2e06ceb86c6064d1397b6405f99d527d49d
seredokhov <seredokhovp@mail.ru>
2018-09-14 19:07:44 +0300

    Прелоадер

--- js/main.js
+++ js/main.js
@@ -1,33 +1,29 @@
 // Прелоадер
 
-(function(){
-	$(window).on('load', function(){
-		var images = document.images;
-		var images_total_count = images.length;
-		var images_loaded_count = 0;
-		var progress = document.getElementById('progress');
+	var images = document.images;
+	var images_total_count = images.length;
+	var images_loaded_count = 0;
+	var progress = document.getElementById('progress');
 
-		for (var i = 0; i < images_total_count; i++) {
-			image_clone = new Image();
-			image_clone.onload = loaded;
-			image_clone.onerror = loaded;
-			image_clone.src = images[i].src;
+	for (var i = 0; i < images_total_count; i++) {
+		image_clone = new Image();
+		image_clone.onload = loaded;
+		image_clone.onerror = loaded;
+		image_clone.src = images[i].src;
 
+	}
+	function loaded() {
+		images_loaded_count++;		
+		progress.innerHTML = ((( 100 / images_total_count ) * images_loaded_count ) << 0 ) + '%';
+
+		if ( images_loaded_count >= images_total_count) {
+			setTimeout(function () {
+				$(".preloader .inner").fadeOut();
+				$(".preloader").fadeOut("slow");
+			}, 500)
 		}
-		function loaded() {
-			images_loaded_count++;		
-			progress.innerHTML = ((( 100 / images_total_count ) * images_loaded_count ) << 0 ) + '%';
 
-			if ( images_loaded_count >= images_total_count) {
-				setTimeout(function () {
-					$(".preloader .inner").fadeOut();
-					$(".preloader").fadeOut("slow");
-				}, 500)
-			}
-
-		}
-	})
-})
+	}
 
 
 
