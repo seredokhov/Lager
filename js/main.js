a2c85051af3c12f02fd19242f35e004c4d070857
seredokhov <seredokhovp@mail.ru>
2018-10-06 12:27:31 +0300

    Оптимизация картинок

--- js/main.js
+++ js/main.js
@@ -5,25 +5,32 @@
 	var images_loaded_count = 0;
 	var progress = document.getElementById('progress');
 
-	for (var i = 0; i < images_total_count; i++) {
-		image_clone = new Image();
-		image_clone.onload = loaded;
-		image_clone.onerror = loaded;
-		image_clone.src = images[i].src;
+	// for (var i = 0; i < images_total_count; i++) {
+	// 	image_clone = new Image();
+	// 	image_clone.onload = loaded;
+	// 	image_clone.onerror = loaded;
+	// 	image_clone.src = images[i].src;
+	// }
+	// function loaded() {
+	// 	images_loaded_count++;		
+	// 	progress.innerHTML = ((( 100 / images_total_count ) * images_loaded_count ) << 0 ) + '%';
 
-	}
-	function loaded() {
-		images_loaded_count++;		
-		progress.innerHTML = ((( 100 / images_total_count ) * images_loaded_count ) << 0 ) + '%';
+	// 	if ( images_loaded_count >= images_total_count) {
+	// 		setTimeout(function () {
+	// 			$(".preloader .inner").fadeOut();
+	// 			$(".preloader").fadeOut("slow");
+	// 		}, 500)
+	// 	}
+	// }
 
-		if ( images_loaded_count >= images_total_count) {
-			setTimeout(function () {
-				$(".preloader .inner").fadeOut();
-				$(".preloader").fadeOut("slow");
-			}, 500)
-		}
 
-	}
+	$(window).on('load', function() {
+		setTimeout(function () {
+			$(".preloader .inner").fadeOut();
+			$(".preloader").fadeOut("slow");
+		}, 500)
+	});
+
 
 
 
